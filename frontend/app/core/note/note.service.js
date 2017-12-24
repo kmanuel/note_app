749c1ba2eb1288dc8fd18ec32dd6945d5b08a6a2
Manuel Kruisz <kruisz88@gmail.com>
2017-12-25 00:41:36 +0100

    use id:note dictionary instead of list

--- frontend/app/core/note/note.service.js
+++ frontend/app/core/note/note.service.js
@@ -2,12 +2,12 @@ angular
     .module('core.note')
     .factory('noteService', ['$http', function ($http) {
         var self = this;
-        this.notes = [];
+        this.notes = {};
         $http.get('http://localhost:8080/note').then(
             function success(response) {
                 for (var index = 0; index < response.data.length; index++) {
                     var note = response.data[index];
-                    self.notes.push(note);
+                    self.notes[note.id] = note;
                 }
             }, function errorCallback(response) {
                 console.error(response);
@@ -20,12 +20,12 @@ angular
             getNotes: function () {
                 return self.notes;
             },
-            getNote: function (noteIdx) {
-                return self.notes[noteIdx];
+            getNote: function (noteId) {
+                return self.notes[noteId];
             },
             addNote: function (newNote) {
-                newNote.id = self.notes.length;
-                self.notes.push(newNote);
+                newNote.id = Object.keys(self.notes).length + 1;
+                self.notes[newNote.id] = newNote;
                 return newNote.id;
             }
         }
