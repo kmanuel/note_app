624cacaa0bebb162198a8720be05c36cb81f5599
Manuel Kruisz <kruisz88@gmail.com>
2017-12-25 02:23:43 +0100

    remove commandlinerunner for testdata init

--- backend/noteservice/src/main/kotlin/com/clowdscy/noteapp/NoteserviceApplication.kt
+++ backend/noteservice/src/main/kotlin/com/clowdscy/noteapp/NoteserviceApplication.kt
@@ -1,22 +1,10 @@
 package com.clowdscy.noteapp
 
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class NoteserviceApplication {
-    @Bean
-    fun init(noteRepository: NoteRepository) = CommandLineRunner {
-        noteRepository.saveAndFlush(Note("title1", "body1", "some-edit-date"))
-        noteRepository.saveAndFlush(Note("title2", "body2", "some-edit-date"))
-        noteRepository.saveAndFlush(Note("title3", "body3", "some-edit-date"))
-        noteRepository.saveAndFlush(Note("title4", "body4", "some-edit-date"))
-        noteRepository.saveAndFlush(Note("title5", "body5", "some-edit-date"))
-        noteRepository.saveAndFlush(Note("title6", "body6", "some-edit-date"))
-    }
-}
+class NoteserviceApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(NoteserviceApplication::class.java, *args)
