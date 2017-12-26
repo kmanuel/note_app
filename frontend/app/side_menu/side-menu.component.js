e6812da0d173773965fe460f1d671cf7dc5ab561
Manuel Kruisz <kruisz88@gmail.com>
2017-12-26 22:26:51 +0100

    mark active side-menu item

--- frontend/app/side_menu/side-menu.component.js
+++ frontend/app/side_menu/side-menu.component.js
@@ -4,6 +4,8 @@ angular
         templateUrl: 'app/side_menu/side-menu.template.html',
         controller: ['noteService',
             function SideMenuController(noteService) {
+                this.activeItem = 'notes';
+
                 this.upload = function() {
                     console.log('upload called');
                     noteService.upload().then(
@@ -14,6 +16,14 @@ angular
                             console.error('upload failed');
                         }
                     )
+                }
+
+                this.setActive = function(item) {
+                    this.activeItem = item;
+                }
+
+                this.isActive = function(item) {
+                    return item === this.activeItem;
                 }
             }]
     });
